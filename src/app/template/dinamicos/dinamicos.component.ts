862fd7fe9fd0bf4b09430b85a7a46926683da1c4
Leyder Quintero <lele.quinter@gmail.com>
2022-10-20 21:39:16 -0500

    Agregar juegos favoritos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -17,6 +17,8 @@ interface Favorito {
 })
 export class DinamicosComponent {
 
+  nuevoJuego: string = '';
+
   persona: Persona = {
     nombre: 'Fernando',
     favoritos: [
@@ -33,6 +35,16 @@ export class DinamicosComponent {
 
   eliminar(index: number) {
     this.persona.favoritos.splice(index, 1);
+  }
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego,
+    }
+
+    this.persona.favoritos.push({ ...nuevoFavorito });
+    this.nuevoJuego = '';
   }
 
 }
